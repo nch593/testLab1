766305f8312636ef77e1c9b80033c813d02b4995
nch593 <158087957+nch593@users.noreply.github.com>
2024-02-01 22:22:32 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+#region Working with single-dimensional arrays
+
+string[] names; // This can reference any size array of strings.
+
+// Allocate memory for four strings in an array.
+names = new string[4];
+
+// Store items at these index positions.
+names[0] = "A";
+names[1] = "B";
+names[2] = "C";
+names[3] = "D";
+
+// Alternative syntax for creating and initializing an array.
+string[] names2 = { "A", "B", "C", "D" };
+
+// Loop through the names.
+for (int i = 0; i < names2.Length; i++)
+{
+  // Output the item at index position i.
+  WriteLine($"{names2[i]} is at position {i}.");
+}
+
+#endregion
+
+#region Working with multi-dimensional arrays
+
+string[,] grid1 = // Two dimensional array.
+{
+  { "Alpha1", "Beta2", "Gamma3", "Delta4" },
+  { "Anne1", "Ben2", "Charlie3", "Doug4" },
+  { "Aardvark1", "Bear2", "Cat3", "Dog4" }
+};
+
+WriteLine($"1st dimension, lower bound: {grid1.GetLowerBound(0)}");
+WriteLine($"1st dimension, upper bound: {grid1.GetUpperBound(0)}");
+WriteLine($"2nd dimension, lower bound: {grid1.GetLowerBound(1)}");
+WriteLine($"2nd dimension, upper bound: {grid1.GetUpperBound(1)}");
+
+for (int row = 0; row <= grid1.GetUpperBound(0); row++)
+{
+  for (int col = 0; col <= grid1.GetUpperBound(1); col++)
+  {
+    WriteLine($"Row {row}, Column {col}: {grid1[row, col]}");
+  }
+}
+
+#endregion
+
+#region Working with jagged arrays
+
+string[][] jagged = // An array of string arrays.
+{
+  new[] { "Alpha11", "Beta22", "Gamma33" },
+  new[] { "Anne11", "Ben22", "Charlie33", "Doug44" },
+  new[] { "Aardvark11", "Bear22" }
+};
+
+WriteLine("Upper bound of the array of arrays is: {0}",
+  jagged.GetUpperBound(0));
+
+for (int array = 0; array <= jagged.GetUpperBound(0); array++)
+{
+  WriteLine("Upper bound of array {0} is: {1}",
+    arg0: array,
+    arg1: jagged[array].GetUpperBound(0));
+}
+
+for (int row = 0; row <= jagged.GetUpperBound(0); row++)
+{
+  for (int col = 0; col <= jagged[row].GetUpperBound(0); col++)
+  {
+    WriteLine($"Row {row}, Column {col}: {jagged[row][col]}");
+  }
+}
+
+#endregion
+
+#region List pattern matching with arrays
+
+int[] sequentialNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int[] oneTwoNumbers = { 1, 2 };
+int[] oneTwoTenNumbers = { 1, 2, 10 };
+int[] oneTwoThreeTenNumbers = { 1, 2, 3, 10 };
+int[] primeNumbers = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+int[] fibonacciNumbers = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 };
+int[] emptyNumbers = { }; // Or use Array.Empty<int>()
+int[] threeNumbers = { 9, 7, 5 };
+int[] sixNumbers = { 9, 7, 5, 4, 2, 10 };
+
+WriteLine($"{nameof(sequentialNumbers)}: {CheckSwitch(sequentialNumbers)}");
+WriteLine($"{nameof(oneTwoNumbers)}: {CheckSwitch(oneTwoNumbers)}");
+WriteLine($"{nameof(oneTwoTenNumbers)}: {CheckSwitch(oneTwoTenNumbers)}");
+WriteLine($"{nameof(oneTwoThreeTenNumbers)}: {CheckSwitch(oneTwoThreeTenNumbers)}");
+WriteLine($"{nameof(primeNumbers)}: {CheckSwitch(primeNumbers)}");
+WriteLine($"{nameof(fibonacciNumbers)}: {CheckSwitch(fibonacciNumbers)}");
+WriteLine($"{nameof(emptyNumbers)}: {CheckSwitch(emptyNumbers)}");
+WriteLine($"{nameof(threeNumbers)}: {CheckSwitch(threeNumbers)}");
+WriteLine($"{nameof(sixNumbers)}: {CheckSwitch(sixNumbers)}");
+
+static string CheckSwitch(int[] values) => values switch
+{
+  [] => "Empty array",
+  [1, 2, _, 10] => "Contains 1, 2, any single number, 10.",
+  [1, 2, .., 10] => "Contains 1, 2, any range including empty, 10.",
+  [1, 2] => "Contains 1 then 2.",
+  [int item1, int item2, int item3] =>
+    $"Contains {item1} then {item2} then {item3}.",
+  [0, _] => "Starts with 0, then one other number.",
+  [0, ..] => "Starts with 0, then any range of numbers.",
+  [2, .. int[] others] => $"Starts with 2, then {others.Length} more numbers.",
+  [..] => "Any items in any order.",
+};
+
+string text = System.IO.File.ReadAllText("text.txt");
+Console.WriteLine(text);
+
+Console.WriteLine("Hello Nakul");
+Console.WriteLine("Version: {0}", Environment.Version.ToString());
+
+#endregion
